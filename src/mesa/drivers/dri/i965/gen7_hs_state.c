a63f1e5517e5ef28d62e97dbe553c45f398c61b9
Fabian Bieler <fabianbieler@fastmail.fm>
2014-03-07 14:02:08 +0100

    i965: Set tessellation state.
    Conflicts:
    	src/mesa/drivers/dri/i965/brw_wm_sampler_state.c
    	src/mesa/drivers/dri/i965/brw_wm_surface_state.c

---
+++ src/mesa/drivers/dri/i965/gen7_hs_state.c
@@ -0,0 +1,163 @@
+/*
+ * Copyright © 2014 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "brw_context.h"
+#include "brw_state.h"
+#include "brw_defines.h"
+#include "intel_batchbuffer.h"
+
+static void
+gen7_upload_hs_push_constants(struct brw_context *brw)
+{
+   struct brw_stage_state *stage_state = &brw->hs.base;
+   /* BRW_NEW_TESS_CTRL_PROGRAM */
+   const struct brw_tess_ctrl_program *hp =
+      (struct brw_tess_ctrl_program *) brw->tess_ctrl_program;
+
+   if (hp) {
+      /* CACHE_NEW_HS_PROG */
+      const struct brw_stage_prog_data *prog_data = &brw->hs.prog_data->base.base;
+      gen6_upload_push_constants(brw, &hp->program.Base, prog_data,
+                                      stage_state, AUB_TRACE_VS_CONSTANTS);
+   }
+
+   gen7_upload_constant_state(brw, stage_state, hp, _3DSTATE_CONSTANT_HS);
+}
+
+const struct brw_tracked_state gen7_hs_push_constants = {
+   .dirty = {
+      .mesa  = _NEW_TRANSFORM | _NEW_PROGRAM_CONSTANTS,
+      .brw   = (BRW_NEW_BATCH |
+                BRW_NEW_TESS_CTRL_PROGRAM |
+                BRW_NEW_PUSH_CONSTANT_ALLOCATION),
+      .cache = CACHE_NEW_HS_PROG,
+   },
+   .emit = gen7_upload_hs_push_constants,
+};
+
+
+static void
+upload_hs_state(struct brw_context *brw)
+{
+   const struct brw_stage_state *stage_state = &brw->hs.base;
+   /* BRW_NEW_TESS_CTRL_PROGRAM */
+   bool active = brw->tess_ctrl_program;
+   if (active)
+      assert(brw->tess_eval_program);
+   /* CACHE_NEW_HS_PROG */
+   const struct brw_vec4_prog_data *prog_data = &brw->hs.prog_data->base;
+
+   /* BRW_NEW_HS_BINDING_TABLE */
+   BEGIN_BATCH(2);
+   OUT_BATCH(_3DSTATE_BINDING_TABLE_POINTERS_HS << 16 | (2 - 2));
+   OUT_BATCH(stage_state->bind_bo_offset);
+   ADVANCE_BATCH();
+
+   /* CACHE_NEW_SAMPLER */
+   BEGIN_BATCH(2);
+   OUT_BATCH(_3DSTATE_SAMPLER_STATE_POINTERS_HS << 16 | (2 - 2));
+   OUT_BATCH(stage_state->sampler_offset);
+   ADVANCE_BATCH();
+
+   gen7_upload_constant_state(brw, stage_state, active, _3DSTATE_CONSTANT_HS);
+
+   // XXX: ivb gt2 gs state requires a cs stall flush here. is this true for hs, too?
+
+   if (active) {
+      BEGIN_BATCH(7);
+      OUT_BATCH(_3DSTATE_HS << 16 | (7 - 2));
+      // XXX: 4.4, wrt max_hs_threads:
+      /* A URB_FENCE command must be issued subsequent to any change to the value in this field and
+         before any subsequent pipeline processing (e.g., via 3DPRIMITIVE or CONSTANT_BUFFER). See
+         Graphics Processing Engine (Command Ordering Rules).*/
+      //brw->state.dirty.brw |= BRW_NEW_URB_FENCE;
+      OUT_BATCH(((ALIGN(stage_state->sampler_count, 4)/4) <<
+                 GEN7_HS_SAMPLER_COUNT_SHIFT) |
+                ((brw->hs.prog_data->base.base.binding_table.size_bytes / 4) <<
+                 GEN7_HS_BINDING_TABLE_ENTRY_COUNT_SHIFT) |
+                ((brw->max_hs_threads - 1) << GEN7_HS_MAX_THREADS_SHIFT));
+      // We operate in simd4x2
+      const int num_instances = (brw->hs.prog_data->instances + 1) / 2;
+      assert(num_instances > 0);
+      assert(num_instances <= 16);
+      /* XXX: If the HS kernel uses a barrier function, software must restrict the Instance Count to the number of
+       * threads that can be simultaneously active within a half-slice. Factors which must be considered
+       * includes scratch memory availability.
+       */
+      OUT_BATCH(GEN7_HS_ENABLE |
+                GEN7_HS_STATISTICS_ENABLE |
+                ((num_instances - 1) <<
+                 GEN7_HS_INSTANCE_CONTROL_SHIFT));
+      OUT_BATCH(stage_state->prog_offset);
+      if (brw->hs.prog_data->base.total_scratch) {
+         OUT_RELOC(stage_state->scratch_bo,
+                   I915_GEM_DOMAIN_RENDER, I915_GEM_DOMAIN_RENDER,
+                   ffs(brw->hs.prog_data->base.total_scratch) - 11);
+      } else {
+         OUT_BATCH(0);
+      }
+
+      uint32_t dw5 =
+      //include vertex handles?
+         (prog_data->base.dispatch_grf_start_reg <<
+          GEN7_HS_DISPATCH_START_GRF_SHIFT) |
+         (prog_data->urb_read_length <<
+          GEN7_HS_URB_READ_LENGTH_SHIFT) |
+         (0 << GEN7_HS_URB_ENTRY_READ_OFFSET_SHIFT);
+
+      uint32_t dw6 = 0;
+
+      OUT_BATCH(dw5);
+      OUT_BATCH(dw6);
+      ADVANCE_BATCH();
+   } else {
+      BEGIN_BATCH(7);
+      OUT_BATCH(_3DSTATE_HS << 16 | (7 - 2));
+      OUT_BATCH((0 << GEN7_HS_SAMPLER_COUNT_SHIFT) |
+                (0 << GEN7_HS_BINDING_TABLE_ENTRY_COUNT_SHIFT) |
+                (0 << GEN7_HS_MAX_THREADS_SHIFT));
+      OUT_BATCH(GEN7_HS_STATISTICS_ENABLE);
+      OUT_BATCH(0); /* prog_bo */
+      OUT_BATCH(0); /* scratch space base offset */
+      OUT_BATCH((1 << GEN7_HS_DISPATCH_START_GRF_SHIFT) |
+                (0 << GEN7_HS_URB_READ_LENGTH_SHIFT) |
+                GEN7_HS_INCLUDE_VERTEX_HANDLES |
+                (0 << GEN7_HS_URB_ENTRY_READ_OFFSET_SHIFT));
+      OUT_BATCH(0);
+      ADVANCE_BATCH();
+   }
+   brw->hs.enabled = active;
+}
+
+const struct brw_tracked_state gen7_hs_state = {
+   .dirty = {
+      .mesa  = _NEW_TRANSFORM | _NEW_PROGRAM_CONSTANTS,
+      .brw   = (BRW_NEW_CONTEXT |
+                BRW_NEW_TESS_CTRL_PROGRAM |
+                BRW_NEW_HS_BINDING_TABLE |
+                BRW_NEW_BATCH |
+                BRW_NEW_PUSH_CONSTANT_ALLOCATION),
+      .cache = CACHE_NEW_HS_PROG | CACHE_NEW_SAMPLER
+   },
+   .emit = upload_hs_state,
+};
