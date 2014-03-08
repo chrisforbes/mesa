88e85c1c52ff1ffc813bd625daec941a9ddc15ad
Fabian Bieler <fabianbieler@fastmail.fm>
2014-03-08 21:56:32 +0100

    i965: Add PATCHLIST_n to primitives

--- src/mesa/drivers/dri/i965/brw_defines.h
+++ src/mesa/drivers/dri/i965/brw_defines.h
@@ -76,6 +76,7 @@
 #define _3DPRIM_LINESTRIP_BF      0x13
 #define _3DPRIM_LINESTRIP_CONT_BF 0x14
 #define _3DPRIM_TRIFAN_NOSTIPPLE  0x15
+#define _3DPRIM_PATCHLIST_0       0x1F
 
 /* We use this offset to be able to pass native primitive types in struct
  * _mesa_prim::mode.  Native primitive types are BRW_PRIM_OFFSET +
