d49483819f9ac74ce0b2ef45095ff351e3d55055
Ilia Mirkin <imirkin@alum.mit.edu>
2014-07-12 23:28:54 -0400

    glsl: keep track of which inputs/outputs are per-patch

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -2324,6 +2324,8 @@ struct gl_tess_ctrl_program
 {
    struct gl_program Base;   /**< base class */
 
+   GLbitfield64 IsPatch;
+
    /* output layout */
    GLint VerticesOut;
 };
@@ -2334,6 +2336,7 @@ struct gl_tess_eval_program
 {
    struct gl_program Base;   /**< base class */
 
+   GLbitfield64 IsPatch;
    /* input layout */
    GLenum PrimitiveMode; /* GL_TRIANGLES, GL_QUADS or GL_ISOLINES */
    GLenum Spacing;       /* GL_EQUAL, GL_FRACTIONAL_EVEN or
