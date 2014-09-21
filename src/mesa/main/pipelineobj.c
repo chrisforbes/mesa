cef06d213557d834d17f06ba7f0f7c26b22f0d9a
Chris Forbes <chrisf@ijw.co.nz>
2014-09-21 12:08:22 +1200

    mesa: Require VS if TCS or TES is present in pipeline
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>

--- src/mesa/main/pipelineobj.c
+++ src/mesa/main/pipelineobj.c
@@ -767,11 +767,11 @@ _mesa_validate_program_pipeline(struct gl_context* ctx,
     *           executable vertex shader."
     */
    if (!pipe->CurrentProgram[MESA_SHADER_VERTEX]
-       && pipe->CurrentProgram[MESA_SHADER_GEOMETRY]) {
+       && (pipe->CurrentProgram[MESA_SHADER_GEOMETRY] ||
+           pipe->CurrentProgram[MESA_SHADER_TESS_CTRL] ||
+           pipe->CurrentProgram[MESA_SHADER_TESS_EVAL])) {
       pipe->InfoLog = ralloc_strdup(pipe, "Program lacks a vertex shader");
       goto err;
-
-      /* XXX: tess */
    }
 
    /* Section 2.11.11 (Shader Execution), subheading "Validation," of the
