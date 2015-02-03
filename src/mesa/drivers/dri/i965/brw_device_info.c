7c892cb7bf3655b6641578e22c1b66afe7cf9d17
Chris Forbes <chrisf@ijw.co.nz>
2015-02-03 19:59:52 +1300

    set max hs threads

--- src/mesa/drivers/dri/i965/brw_device_info.c
+++ src/mesa/drivers/dri/i965/brw_device_info.c
@@ -204,6 +204,7 @@ static const struct brw_device_info brw_device_info_hsw_gt3 = {
    .max_hs_threads = 256,
    .max_ds_threads = 280,
    .max_gs_threads = 256,
+   .max_hs_threads = 36,
    .max_wm_threads = 408,
    .max_ds_threads = 280,
    .urb = {
