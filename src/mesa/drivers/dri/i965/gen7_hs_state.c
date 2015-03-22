77d1866bc75274e12f84aa7b576d5c311b578336
Chris Forbes <chrisf@ijw.co.nz>
2015-03-23 11:14:04 +1300

    hack ahck

--- src/mesa/drivers/dri/i965/gen7_hs_state.c
+++ src/mesa/drivers/dri/i965/gen7_hs_state.c
@@ -34,6 +34,8 @@ gen7_upload_hs_push_constants(struct brw_context *brw)
    const struct brw_tess_ctrl_program *hp =
       (struct brw_tess_ctrl_program *) brw->tess_ctrl_program;
 
+      gen7_emit_cs_stall_flush(brw);
+
    if (hp) {
       /* BRW_NEW_HS_PROG_DATA */
       const struct brw_stage_prog_data *prog_data = &brw->hs.prog_data->base.base;
@@ -41,7 +43,11 @@ gen7_upload_hs_push_constants(struct brw_context *brw)
                                       stage_state, AUB_TRACE_VS_CONSTANTS);
    }
 
+      gen7_emit_cs_stall_flush(brw);
+
    gen7_upload_constant_state(brw, stage_state, hp, _3DSTATE_CONSTANT_HS);
+
+      gen7_emit_cs_stall_flush(brw);
 }
 
 const struct brw_tracked_state gen7_hs_push_constants = {
