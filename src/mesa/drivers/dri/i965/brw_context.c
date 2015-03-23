37fa57a232b237aa06219eb2fc4ff2a854848615
Chris Forbes <chrisf@ijw.co.nz>
2015-03-23 15:04:22 +1300

    set stage ids for tess stages

--- src/mesa/drivers/dri/i965/brw_context.c
+++ src/mesa/drivers/dri/i965/brw_context.c
@@ -735,6 +735,8 @@ brwCreateContext(gl_api api,
    brw->has_swizzling = screen->hw_has_swizzling;
 
    brw->vs.base.stage = MESA_SHADER_VERTEX;
+   brw->hs.base.stage = MESA_SHADER_TESS_CTRL;
+   brw->ds.base.stage = MESA_SHADER_TESS_EVAL;
    brw->gs.base.stage = MESA_SHADER_GEOMETRY;
    brw->wm.base.stage = MESA_SHADER_FRAGMENT;
    if (brw->gen >= 8) {
